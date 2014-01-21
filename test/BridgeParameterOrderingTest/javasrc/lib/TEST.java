f89a57a523259f566864a63818795d924650b02b
Travis London <travis.london@gmail.com>
2014-01-21 15:45:19 -0700

    Job: #153 Add test model to repository

---
+++ test/BridgeParameterOrderingTest/javasrc/lib/TEST.java
@@ -0,0 +1,13 @@
+package lib;
+public class TEST {
+
+	public static String hello_world() {
+		return "Hello world";
+	}
+
+	public static String concat_string(String s1, String s2) {
+		return s1 + s2;
+	}
+
+}
+
