15aae04d0a4cb48c63b4c036a91cd2220e273a3c
Travis London <travis.london@gmail.com>
2014-02-17 12:27:44 -0700

    Job: #158 Add test model

---
+++ test/ExternalEntityRealizedTests/javasrc/lib/LOG.java
@@ -0,0 +1,41 @@
+//========================================================================
+//
+//File:      $RCSfile$
+//Version:   $Revision$
+//Modified:  $Date$
+//
+//(c) Copyright 2006-2014 by Mentor Graphics Corp. All rights reserved.
+//
+//========================================================================
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+// use this file except in compliance with the License.  You may obtain a copy 
+// of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   See the 
+// License for the specific language governing permissions and limitations under
+// the License.
+//======================================================================== 
+
+package lib;
+
+import com.mentor.nucleus.bp.core.CorePlugin;
+
+public class LOG {
+
+	public static void LogInfo(String ee_message) {
+		CorePlugin.out.println("LogInfo:  " + ee_message);
+	}
+
+	public static boolean LogInfoWithReturn(String ee_message) {
+		CorePlugin.out.println("LogInfoWithReturn: " + ee_message);
+		return true;
+	}
+
+	public static void LogResult(boolean result) {
+		CorePlugin.out.println("LogResult: " + result);
+	}
+}
