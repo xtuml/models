4a1633a617d23b84a3804abbcf93343358164bf9
Levi Starrett <levi@roxsoftware.com>
2017-05-30 10:27:49 -0400

    job #9538 working locally

--- applications/webchat/gen/user_CHAT_bridge.c
+++ applications/webchat/gen/user_CHAT_bridge.c
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
 
 #define STDIN 0
 
@@ -58,19 +59,31 @@ user_CHAT_input( i_t * p_error, c_t p_input[ESCHER_SYS_MAX_STRING_LEN] )
 i_t
 user_CHAT_checkread( i_t * p_error )
 {
-  *p_error = 35;
-  return -1;
+  // clear fd sets
+  fd_set readfds;
+  FD_ZERO( &readfds );
+  FD_SET( STDIN, &readfds );
+
+  // set timeout of 100ms (non-blocking)
+  struct timeval timeout;
+  memset( &timeout, 0, sizeof(struct timeval) );
+  timeout.tv_usec = 100000;
+
+  // call select
+  i_t ret_val = select( STDIN+1, &readfds, NULL, NULL, &timeout );
+  if ( -1 == ret_val ) {
+    *p_error = errno;
+    return ret_val;
+  }
+
+  // check if is readable
+  if ( FD_ISSET( STDIN, &readfds ) ) {
+    // no errors filedes is readable
+    *p_error = 0;
+    return 0;
+  }
+  else {
+    *p_error = EWOULDBLOCK;
+    return -1;
+  }
 }
-
-
-/*
- * Bridge:  read
- */
-i_t
-user_CHAT_read( c_t p_data[ESCHER_SYS_MAX_STRING_LEN], i_t * p_error )
-{
-  i_t result = 0;
-  /* Insert your implementation code here... */
-  return result;
-}
-
