97707b3a2095c44b7a8ceb0b79a70882aa85a24a
Levi Starrett <levi@roxsoftware.com>
2018-03-25 22:36:37 -0400

    job #10054 first pass converting Location domain

---
+++ applications/gps/GPSoal/Location/gen/generate-masl.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath masl/Location:../Tracking/masl/Tracking:../../../../masl/EEs/LOG/masl/LOG -mod masl/Location/Location.mod
