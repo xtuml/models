6b054577d3c6cae27f86dd53101f502bc93c177d
Levi Starrett <levi@roxsoftware.com>
2018-03-28 16:36:50 -0400

    job #10054 setting up GPS_Watch project

--- applications/gps/GPSoal/GPS_Watch/gen/generate-masl.sh
+++ applications/gps/GPSoal/GPS_Watch/gen/generate-masl.sh
@@ -19,10 +19,10 @@ fi
 GEN_GPS_Watch_Logger_LOC="$HOME/git/masl/examples/utils/Logger_OOA"
 GEN_GPS_Watch_Format_LOC="$HOME/git/masl/examples/utils/Format_OOA"
 $MASLMC -domainpath $GEN_GPS_Watch_UI_LOC:$GEN_GPS_Watch_Location_LOC:$GEN_GPS_Watch_HeartRateMonitor_LOC:$GEN_GPS_Watch_Tracking_LOC:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -prj $CWD/masl/GPS_Watch/GPS_Watch.prj -output $CWD/gen/code_generation/GPS_Watch
-$MASLMC -domainpath $CWD/masl/UI:$GEN_GPS_Watch_Tracking_LOC:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $CWD/masl/UI/UI.mod -output $CWD/gen/code_generation/UI
-$MASLMC -domainpath $CWD/masl/Location:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $CWD/masl/Location/Location.mod -output $CWD/gen/code_generation/Location
-$MASLMC -domainpath $CWD/masl/HeartRateMonitor:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $CWD/masl/HeartRateMonitor/HeartRateMonitor.mod -output $CWD/gen/code_generation/HeartRateMonitor
-$MASLMC -domainpath $CWD/masl/Tracking:$GEN_GPS_Watch_UI_LOC:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $CWD/masl/Tracking/Tracking.mod -output $CWD/gen/code_generation/Tracking
+$MASLMC -domainpath $CWD/masl/UI:$GEN_GPS_Watch_Tracking_LOC:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $GEN_GPS_Watch_UI_LOC/UI.mod -output $CWD/gen/code_generation/UI
+$MASLMC -domainpath $CWD/masl/Location:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $GEN_GPS_Watch_Location_LOC/Location.mod -output $CWD/gen/code_generation/Location
+$MASLMC -domainpath $CWD/masl/HeartRateMonitor:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $GEN_GPS_Watch_HeartRateMonitor_LOC/HeartRateMonitor.mod -output $CWD/gen/code_generation/HeartRateMonitor
+$MASLMC -domainpath $CWD/masl/Tracking:$GEN_GPS_Watch_UI_LOC:$GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $GEN_GPS_Watch_Tracking_LOC/Tracking.mod -output $CWD/gen/code_generation/Tracking
 $MASLMC -domainpath $GEN_GPS_Watch_LOG_LOC:$GEN_GPS_Watch_Logger_LOC:$GEN_GPS_Watch_Format_LOC -mod $GEN_GPS_Watch_LOG_LOC/LOG.int -output $CWD/gen/code_generation/LOG
 $MASLMC -domainpath $GEN_GPS_Watch_Logger_LOC -mod $GEN_GPS_Watch_Logger_LOC/Logger.int -output $CWD/gen/code_generation/Logger
 $MASLMC -domainpath $GEN_GPS_Watch_Format_LOC -mod $GEN_GPS_Watch_Format_LOC/Format.int -output $CWD/gen/code_generation/Format
