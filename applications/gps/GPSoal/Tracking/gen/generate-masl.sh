aa1f60aa5b1ad84b21a4108bca8f8daad45bcafc
Levi Starrett <levi@roxsoftware.com>
2018-03-28 11:49:25 -0400

    job #10054 first pass converting Tracking to MASL. No action language has been updated yet

--- applications/gps/GPSoal/Tracking/gen/generate-masl.sh
+++ applications/gps/GPSoal/Tracking/gen/generate-masl.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath masl/LOG:$HOME/git/masl/examples/utils -mod masl/LOG/LOG.mod
+MASLMC=$HOME/git/masl/core-java/install/masl-core/bin/masl-core
+CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+if [[ "" == $GEN_Tracking_LOG_LOC ]]; then
+  GEN_Tracking_LOG_LOC="$CWD/../../../../masl/EEs/LOG/masl/LOG"
+fi
+if [[ "" == $GEN_Tracking_LOG_LOC ]]; then
+  GEN_Tracking_UI_LOC="$CWD/../UI/masl/UI"
+fi
+GEN_Tracking_Logger_LOC="$HOME/git/masl/examples/utils/Logger_OOA"
+GEN_Tracking_Format_LOC="$HOME/git/masl/examples/utils/Format_OOA"
+$MASLMC -domainpath $CWD/masl/Tracking:$GEN_Tracking_LOG_LOC:$GEN_Tracking_Logger_LOC:$GEN_Tracking_Format_LOC -mod $CWD/masl/Tracking/Tracking.mod -output $CWD/gen/code_generation/Tracking
+$MASLMC -domainpath $GEN_Tracking_UI_LOC -mod $GEN_Tracking_UI_LOC/UI.int -output $CWD/gen/code_generation/UI
+$MASLMC -domainpath $GEN_Tracking_LOG_LOC:$GEN_Tracking_Logger_LOC:$GEN_Tracking_Format_LOC -mod $GEN_Tracking_LOG_LOC/LOG.int -output $CWD/gen/code_generation/LOG
+$MASLMC -domainpath $GEN_Tracking_Logger_LOC -mod $GEN_Tracking_Logger_LOC/Logger.int -output $CWD/gen/code_generation/Logger
+$MASLMC -domainpath $GEN_Tracking_Format_LOC -mod $GEN_Tracking_Format_LOC/Format.int -output $CWD/gen/code_generation/Format
+cd $CWD
+cmake . -DCMAKE_INSTALL_PREFIX=$PWD
--- applications/gps/GPSoal/Tracking/masl/Tracking/Tracking.int
+++
@@ -1,4 +0,0 @@
-domain Tracking is
-  public type GoalCriteria is enum ( HeartRate, Pace ); 
-  public type GoalSpan is enum ( Distance, Time ); 
-end domain;
