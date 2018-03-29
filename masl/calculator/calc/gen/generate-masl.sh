259499319c86c4479fb6f3f829d76fbc64f23974
Levi Starrett <levi@roxsoftware.com>
2018-03-29 11:35:51 -0400

    job #10166 renamed 'calc' to 'ALU'

--- masl/calculator/calc/gen/generate-masl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-MASLMC=$HOME/git/masl/core-java/install/masl-core/bin/masl-core
-CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-$MASLMC -domainpath $CWD/masl/calc -mod $CWD/masl/calc/calc.mod -output $CWD/gen/code_generation/calc -test
-cd $CWD
-cmake . -DCMAKE_INSTALL_PREFIX=$PWD
