0cc96583bc34dbd49357b6ff67bff37393af9a6a
Levi Starrett <levi@roxsoftware.com>
2018-03-29 11:02:50 -0400

    job #10166 removed 'calc_test' domain

--- masl/calculator/calc_test/gen/generate-masl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-MASLMC=$HOME/git/masl/core-java/install/masl-core/bin/masl-core
-CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-$MASLMC -domainpath $CWD/masl/calc_test -mod $CWD/masl/calc_test/calc_test.mod -output $CWD/gen/code_generation/calc_test
-cd $CWD
-cmake . -DCMAKE_INSTALL_PREFIX=$PWD
--- masl/calculator/calculator/gen/copy_ints.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp ../../calc/masl/calc/calc.int ../models/calculator/calculator/calculator
-cp ../../calc_test/masl/calc_test/calc_test.int ../models/calculator/calculator/calculator
-cp ../../keypad/masl/keypad/keypad.int ../models/calculator/calculator/calculator
