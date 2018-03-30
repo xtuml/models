9fcb1b08bfc1c05c2652c9b4dcb71663c60a5641
Levi Starrett <levi@roxsoftware.com>
2018-03-30 14:58:44 -0400

    job #10054 update MATH domain to build with cmake

--- masl/EEs/MATH/gen/generate-masl.sh
+++ masl/EEs/MATH/gen/generate-masl.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath masl/MATH:$HOME/git/masl/examples/utils -mod masl/MATH/MATH.mod
+CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+GEN_MATH_Math_LOC="$HOME/git/masl/examples/utils/Math_OOA"
+$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath $CWD/masl/MATH:$GEN_MATH_Math_LOC -mod $CWD/masl/MATH/MATH.mod -output $CWD/gen/code_generation/MATH
+$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath $GEN_MATH_Math_LOC -mod $GEN_MATH_Math_LOC/Math.int -output $CWD/gen/code_generation/Math
+cmake $CWD -DCMAKE_INSTALL_PREFIX=$PWD
