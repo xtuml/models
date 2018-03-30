b1b583187fc58139ea746a203d82452b438583b5
Levi Starrett <levi@roxsoftware.com>
2018-03-30 15:10:03 -0400

    job #10054 updated ARCH domain to build with cmake

--- masl/EEs/ARCH/gen/generate-masl.sh
+++ masl/EEs/ARCH/gen/generate-masl.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath masl/ARCH -mod masl/ARCH/ARCH.mod
+CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath $CWD/masl/ARCH -mod $CWD/masl/ARCH/ARCH.mod -output $CWD/gen/code_generation/ARCH
+cmake $CWD -DCMAKE_INSTALL_PREFIX=$PWD
