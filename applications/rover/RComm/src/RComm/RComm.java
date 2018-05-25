7f7ee5b4a036bf495f5465ea03731d73d87cd7aa
Levi Starrett <levi@roxsoftware.com>
2018-05-25 15:20:09 -0400

    job #10263 simulation working in verifier

--- applications/rover/RComm/src/RComm/RComm.java
+++ applications/rover/RComm/src/RComm/RComm.java
@@ -1,5 +1,16 @@
 package rcomm;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 import org.xtuml.bp.core.ComponentInstance_c;
 import org.xtuml.bp.core.CorePlugin;
 
@@ -12,15 +23,22 @@ public class RComm implements IRCommToProvider, ILocationDataFromProvider {
     
     IRCommFromProvider rcomm_from_provider;
     ILocationDataToProvider location_data_to_provider;
+
+    private static SocketCommunicator rover = new SocketCommunicator();
+    private static SocketCommunicator ref = new SocketCommunicator();
+
+    public static final String IP = "127.0.0.1";
+    public static final String CONFIG_FILE = "/Users/levi/scratch/Challenge problem/Settings/config.txt";
     
     public RComm( IRCommFromProvider rcomm_from_provider, ILocationDataToProvider location_data_to_provider ) {
         this.rcomm_from_provider = rcomm_from_provider;
         this.location_data_to_provider = location_data_to_provider;
+        setup();
     }
 
     @Override
     public void setForwardPower( ComponentInstance_c senderReceiver, int power ) {
-        CorePlugin.out.println( "Setting forward power." );
+        rover.noReply( "Rover,setForwardPower(" + new Integer( power ).toString() + ")" );
     }
 
     @Override
@@ -65,7 +83,94 @@ public class RComm implements IRCommToProvider, ILocationDataFromProvider {
 
     @Override
     public void ready( ComponentInstance_c senderReceiver ) {
-        CorePlugin.out.println( "Sending 'ready'" );
+        ref.noReply( "ready" );
+    }
+
+    public void setup() {
+        int refPort = 0;
+        int roverPort = 0;
+        try{
+            //------ Load config
+            java.util.List<String> conf = readFile( CONFIG_FILE );
+            Iterator<String> conIt = conf.iterator();
+            while (conIt.hasNext()) {
+                String a = conIt.next();
+                if (a.contains("=") == true) {
+                    String b = a.substring(0, a.indexOf('='));
+                    if (b.equals("controlPort"))
+                        roverPort = Integer.parseInt(a.substring(a.indexOf('=')+1));
+                    if (b.equals("observationPort"))
+                        refPort = Integer.parseInt(a.substring(a.indexOf('=')+1));
+                }
+            }
+            rover.connectToServer(IP,roverPort);
+            ref.connectToServer(IP,refPort);
+        }
+        catch (Exception e){}
+    }
+
+    // file read
+    public static java.util.List<String> readFile(String fileName) {
+        File file = new File(fileName);
+        // this gives you a 2-dimensional array of strings
+        java.util.List<String> data = new ArrayList<>();
+        Scanner inputStream;
+        try {
+            inputStream = new Scanner(file);
+
+            while (inputStream.hasNext()) {
+                data.add(inputStream.next());
+            }
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace( CorePlugin.err );
+        }
+        return data;
+    }
+
+    static class SocketCommunicator {
+
+        private BufferedReader in;
+        private PrintWriter out = null;
+        private Socket socket = null;
+
+        public SocketCommunicator() {
+        }
+
+        public void connectToServer(String IP, int port) throws IOException {
+            // Make connection and initialize streams
+            socket = new Socket(IP, port);
+            in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            CorePlugin.out.println("connected");
+        }
+
+        // Send command for sending a message through the socket and returning the reply
+        public String send(String message) {
+            String reply = null;
+            if (out == null){
+              CorePlugin.out.println("fail - socket error");
+            }
+            out.println(message);
+            try {
+                reply = in.readLine();
+            }
+            catch(java.io.IOException e){ e.printStackTrace( CorePlugin.err );}
+            return reply;
+        }
+        // expecting no reply.
+        public void noReply(String message){
+          out.println(message);
+        }
+
+        // Closes the connection on the socket
+        public void close(){
+            try {
+                socket.close();
+            }
+            catch(java.io.IOException e){ e.printStackTrace( CorePlugin.err );}
+        }
     }
 
 }
