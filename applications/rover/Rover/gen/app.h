2d27b44f272977f84197b3cd4f172e56647c30ce
Levi Starrett <levi@roxsoftware.com>
2018-08-02 13:15:12 -0400

    job #10263 rover application working on MCLM

--- applications/rover/Rover/gen/app.h
+++ applications/rover/Rover/gen/app.h
@@ -1,20 +1,17 @@
-#pragma once                                                                                                                                         
-                                                                                                                                                     
+/**
+ * app.h
+ * EV3 HRP app header
+ */
+
 #include "ev3api.h"                                                                                                                                  
-                                                                                                                                                     
-#define MAIN_PRIORITY 5                                                                                                                              
-#define HIGH_PRIORITY 9                                                                                                                              
-#define MID_PRIORITY  10                                                                                                                             
-#define LOW_PRIORITY  11                                                                                                                             
-                                                                                                                                                     
-#ifndef STACK_SIZE                                                                                                                                   
-#define STACK_SIZE    4096                                                                                                                           
-#endif /* STACK_SIZE */                                                                                                                              
-                                                                                                                                                     
-#ifndef TOPPERS_MACRO_ONLY                                                                                                                           
-                                                                                                                                                     
-#include <stdio.h>                                                                                                                                   
-                                                                                                                                                     
+
+#ifndef TOPPERS_MACRO_ONLY
+
 extern void main_task(intptr_t exinf);
-                                                                                                                                                     
-#endif /* TOPPERS_MACRO_ONLY */  
+
+extern void timer_start(intptr_t extinf);
+extern void cyclic_handler(intptr_t extinf);
+extern void bluetooth_task(intptr_t extinf);
+extern void AlarmMainTIM_tick(void);
+
+#endif
