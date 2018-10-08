d56f723ef3f3f3aa2f35495e752b6e6accffe095
Keith Brown <s.keith.brown@gmail.com>
2018-10-08 09:53:09 -0400

    job #10369 - committing work in progress to add leader bearing calculation

---
+++ applications/rover/Rover/gen/LOG_bridge.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------
+ * File:  LOG_bridge.h
+ *
+ * Description:
+ * Methods for bridging to an external entity.
+ *
+ * External Entity:  Logging (LOG)
+ * 
+ * your copyright statement can go here (from te_copyright.body)
+ *--------------------------------------------------------------------------*/
+
+#ifndef LOG_BRIDGE_H
+#define LOG_BRIDGE_H
+#ifdef	__cplusplus
+extern	"C"	{
+#endif
+
+#include "Rover_sys_types.h"
+void LOG_LogInfo( c_t[ESCHER_SYS_MAX_STRING_LEN] );
+void LOG_LogReal( c_t[ESCHER_SYS_MAX_STRING_LEN], const r_t );
+void LOG_LogInteger( c_t[ESCHER_SYS_MAX_STRING_LEN], const i_t );
+
+#ifdef	__cplusplus
+}
+#endif
+#endif   /* LOG_BRIDGE_H */
