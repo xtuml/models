55284f02f100e9c57115b7517993f9f8afc5d620
amullarney <alasdar@sbcglobal.net>
2021-08-16 15:10:38 -0700

    Initial commit of Verifier test for inst_ref params.
    Tested in 12222 Verifier.
    Ciera test is awaiting 12191, 12002 completion.

---
+++ VandMC_testing/VerifierTest/Verify12222/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp $HOME/.m2/repository/io/ciera/runtime/2.3.1/runtime-2.3.1.jar:$HOME/.m2/repository/org/xtuml/Tests/1.0.0-SNAPSHOT/Tests-1.0.0-SNAPSHOT.jar tests.TestsApplication
