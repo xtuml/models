3e9de5bb89088779f6346cc645867c52bc5025ed
amullarney <alasdar@sbcglobal.net>
2021-11-19 14:00:14 -0800

    Add deferred op
    Updated pom to use MaileTechnical 2.5.2 release.
    Updated run.sh

--- VandMC_testing/VerifierTest/Verify11958/target/generated-sources/java/deployment/shareable/Resource.java
+++
@@ -1,9 +0,0 @@
-package deployment.shareable;
-
-
-import io.ciera.runtime.summit.types.IXtumlType;
-
-
-public interface Resource extends IXtumlType {
-
-}
--- VandMC_testing/VerifierTest/Verify11958/target/generated-sources/java/deployment/shareable/Thing.java
+++
@@ -1,9 +0,0 @@
-package deployment.shareable;
-
-
-import io.ciera.runtime.summit.types.IXtumlType;
-
-
-public interface Thing extends IXtumlType {
-
-}
