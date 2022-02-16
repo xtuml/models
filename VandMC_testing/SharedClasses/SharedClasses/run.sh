103ea8dafceca0fa04693784add7a8af26aac6c8
amullarney <alasdar@sbcglobal.net>
2022-02-16 12:24:44 -0800

    job: #12371 - shared classes test
    Adding comprehensive test: pkg ref; inst_ref msg params

---
+++ VandMC_testing/SharedClasses/SharedClasses/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp $HOME/.m2/repository/io/ciera/runtime/2.5.12002-SNAPSHOT/runtime-2.5.12002-SNAPSHOT.jar:$HOME/.m2/repository/org/xtuml/SharedClasses/1.0.0-SNAPSHOT/SharedClasses-1.0.0-SNAPSHOT.jar:$HOME/.m2/repository/org/json/json/20201115/json-20201115.jar sharedclasses.SharedClassesApplication
