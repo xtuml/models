03dbd8a26b756722a85ed892ae24247a635a3360
amullarney <alasdar@sbcglobal.net>
2022-02-21 14:58:09 -0800

    Add Shared Classes test model A model which can be run in Verifier or compiled with Ciera, demonstrates Package Reference, inst_ref/set port messsage parameters

---
+++ VandMC_testing/SharedClasses/SharedClasses/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp $HOME/.m2/repository/io/ciera/runtime/2.5.3/runtime-2.5.3.jar:$HOME/.m2/repository/org/xtuml/SharedClasses/1.0.0-SNAPSHOT/SharedClasses-1.0.0-SNAPSHOT.jar:$HOME/.m2/repository/org/json/json/20201115/json-20201115.jar sharedclasses.SharedClassesApplication
